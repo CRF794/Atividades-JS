dfd93d5fb589c097a039f195f8de2a9761c90c6f
carlos <carlos.a.reyes.flores@gmail.com>
2024-04-19 11:17:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome = "Carlos Armando Reyes Flores"
+let idade = 29
+let estadoCivil = true
+
+console.log(nome, idade, estadoCivil)
+
+let N1 = 10
+let N2 = 14
+let N3 = 8
+let N4 = 12
+
+console.log((N1+N2+N3+N4)/4)
